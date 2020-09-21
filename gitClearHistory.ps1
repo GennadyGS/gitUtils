53e11935d0e52e8b5c74c767a04f9b91d7e9d340
Gennadii Saltyshchak <GennadyGS@gmail.com>
2020-09-21 12:50:18 +0300

    Add script gitClearHistory.ps1

---
+++ gitClearHistory.ps1
@@ -0,0 +1,25 @@
+param(
+    $remoteName = 'origin'
+)
+
+. $PSScriptRoot/gitUtils.ps1
+
+$remoteUrl = RunGit "config --get remote.origin.url"
+if (!$remoteUrl) {
+    "Not git repository"
+    Exit
+}
+
+$confirmation = Read-Host "Are you sure to clear history for repository $remoteUrl ?"
+if ($confirmation -ne 'Y') { Exit }
+Remove-Item -Recurse -Force .git
+
+RunGit init
+RunGit "add ."
+RunGit "commit -m 'initial commit'"
+
+RunGit "remote add origin $remoteurl"
+
+$confirmation = Read-Host "Are you sure to push changes?"
+if ($confirmation -ne 'Y') { Exit }
+RunGit "push -u --force origin master"
