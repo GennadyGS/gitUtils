6c78711903d2214840e06d060a74755743d5ac44
Gennadii Saltyshchak <GennadyGS@gmail.com>
2022-02-23 18:06:28 +0200

    Add function IsCurrentRepository

--- gitUtils.ps1
+++ gitUtils.ps1
@@ -39,12 +39,26 @@ Function GetCurrentRepositoryName {
     [regex]::match($remoteUrl, ".*/(.*)$").Groups[1].Value
 }
 
+Function IsInsideWorkTree {
+    $output = @(Invoke-Expression "git rev-parse --is-inside-work-tree" -ErrorAction Ignore)
+    ($output | Select-Object -First 1) -eq "true"
+}
+
+Function IsCurrentRepository {
+    param (
+        [Parameter(Mandatory=$true)] $repositoryName,
+        [Parameter(Mandatory=$true)] $remoteName
+    )
+    if (!(IsInsideWorkTree)) { return $false }
+    (GetCurrentRepositoryName $remoteName) -eq $repositoryName
+}
+
 Function GetWorkItems {
     param (
         [Parameter(Mandatory=$true)] $targetBranchName,
         [Parameter(Mandatory=$true)] $sourceBranchName
     )
-    RunGit "log --oneline $targetBranchName..$sourceBranchName --no-merges" `
+    RunGit "log --oneline $targetBranchName..$sourceBranchName --no-merges" -noLog `
         | % { [regex]::match($_, "#(\d+)").Groups[1].Value } `
         | ? { $_ } `
         | Sort-Object -Unique
@@ -55,6 +69,6 @@ Function GetCommitMessages {
         [Parameter(Mandatory=$true)] $targetBranchName,
         [Parameter(Mandatory=$true)] $sourceBranchName
     )
-    RunGit "log --oneline $targetBranchName..$sourceBranchName --no-merges" `
+    RunGit "log --oneline $targetBranchName..$sourceBranchName --no-merges" -noLog `
         | % { [regex]::match($_, "[0-9a-f]{7,12} (.*)").Groups[1].Value } `
 }
