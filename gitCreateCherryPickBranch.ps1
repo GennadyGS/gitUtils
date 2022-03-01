924eedb0e54556a54f2a87f070c30cb00747d41f
Gennadii Saltyshchak <GennadyGS@gmail.com>
2022-03-01 12:37:33 +0200

    Add script

---
+++ gitCreateCherryPickBranch.ps1
@@ -0,0 +1,41 @@
+﻿param(
+    [Parameter(Mandatory=$true)] $toTargetBranchName,
+    $fromTargetBranchName = "master",
+    $sourceBranchName,
+    $remoteName = "origin",
+    [switch] $returnToCurrentBranch
+)
+
+. $PSScriptRoot/gitUtils.ps1
+
+CheckGitStash | Out-Null
+
+$currentBranch = GetCurrentBranch
+"current branch is $currentBranch"
+
+if (!$sourceBranchName) {
+    $sourceBranchName = $currentBranch
+}
+
+if ($sourceBranchName -eq $toTargetBranchName) {
+    "branch $toTargetBranchName is already created"
+    exit
+}
+if ($currentBranch -ne $sourceBranchName) {
+    RunGit "checkout $sourceBranchName"
+}
+
+. $PsScriptRoot\gitRecreateBranch.ps1 $toTargetBranchName
+
+$newBranchName = "$sourceBranchName-to-$toTargetBranchName"
+
+RunGit "checkout -b $newBranchName"
+
+git cherry-pick --abort
+RunGit "cherry-pick $remoteName/$fromTargetBranchName..$sourceBranchName"
+
+RunGit "push -u $remoteName $newBranchName"
+
+if ($returnToCurrentBranch) {
+    RunGit "checkout $currentBranch"
+}
