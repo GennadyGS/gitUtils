c34eacbad558117cd866a336ced504601e2575f3
Gennadii Saltyshchak <GennadyGS@gmail.com>
2022-10-31 10:28:42 +0200

    Support additional args

--- gitAllClone.ps1
+++ gitAllClone.ps1
@@ -1,9 +1,10 @@
 param (
-    [Parameter(mandatory=$true)] $fileName
+    [parameter(mandatory=$true, position=0)][string]$fileName,
+    [parameter(mandatory=$false, position=1, ValueFromRemainingArguments=$true)]$remainingArgs
 )
 
 . $PSScriptRoot/gitUtils.ps1
 
 Get-Content $fileName `
     | ? { $_ } `
-    | % { RunGit "clone $_ $args" }
+    | % { RunGit "clone $_ $remainingArgs" }
