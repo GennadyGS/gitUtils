c98e21da41d6d010e39ef21d73dc27887f2b41f9
Gennadii Saltyshchak <GennadyGS@gmail.com>
2022-12-27 08:07:53 +0200

    Add option deleteCurrentBranch

--- gitRecreateBranch.ps1
+++ gitRecreateBranch.ps1
@@ -1,8 +1,15 @@
 ﻿param(
-    [Parameter(Mandatory=$true)] $targetBranchName
+    [Parameter(Mandatory=$true)] $targetBranchName,
+    [switch] $deleteCurrentBranch
 )
 
 . $PSScriptRoot/gitUtils.ps1
 
+$currentBranch = GetCurrentBranch
+
 git branch -d $targetBranchName
 RunGit "checkout $targetBranchName"
+
+if ($deleteCurrentBranch) {
+    git branch -d $currentBranch
+}
