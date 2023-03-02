a133219f16df25f2369ae51fb456b5a8f8018cf1
Gennadii Saltyshchak <GennadyGS@gmail.com>
2023-03-02 08:49:24 +0200

    Support multiple main branch names in gitAllStatus.ps1

--- gitAllStatus.ps1
+++ gitAllStatus.ps1
@@ -1,25 +1,45 @@
 param (
-    $remoteName = "origin",
-    $branchName = "master"
+    [string[]] $mainbranchNames = @(),
+    $remoteName = "origin"
 )
 
 . $PSScriptRoot/gitUtils.ps1
 
-$untouchedMessage = "On branch $branchName Your branch is up to date with '$remoteName/$branchName'.  nothing to commit, working tree clean"
+$wellKnownMainBranchNames = @("main", "master")
+$mainBranchNamesFileName = "mainBranchNames.txt"
+
+$establishedMainBranchNames = $wellKnownMainBranchNames + $mainBranchNames
+if (Test-Path $mainBranchNamesFileName) {
+    $establishedMainBranchNames += Get-Content $mainBranchNamesFileName
+}
+
+Function IsUntouchedMessage() {
+    param([string] $message)
+
+    foreach ($mainBranchName in $establishedMainBranchNames) {
+        $untouchedMessage =
+        "On branch $mainBranchName Your branch is up to date with '$remoteName/$mainBranchName'.  " `
+        + "nothing to commit, working tree clean"
+        if ($message.Contains($untouchedMessage)) {
+            return $true
+        }
+    }
+    return $false
+}
 
 $gitDirectoryName = ".git"
-If (!(Test-Path ".\$gitDirectoryName")) {
+if (!(Test-Path ".\$gitDirectoryName")) {
     Get-ChildItem -Directory `
-        | ? {Test-Path "$_\$gitDirectoryName"} `
-        | % {
-            Write-Host "$_>" -NoNewLine -ForegroundColor darkYellow
-            Push-Location $_
-            $result = RunGit status
-            if (!(([string]$result).Contains($untouchedMessage))) {
-                RunGit status
-            }
-            Pop-Location
+    | Where-Object { Test-Path "$_\$gitDirectoryName" } `
+    | ForEach-Object {
+        Write-Host "$_>" -NoNewLine -ForegroundColor darkYellow
+        Push-Location $_
+        $result = RunGit status
+        if (!(IsUntouchedMessage($result))) {
+            RunGit status
         }
-} Else {
+        Pop-Location
+    }
+} else {
     RunGit status
 }
